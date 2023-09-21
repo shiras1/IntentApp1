45d3891bdcf7f6302850849c1fb62f5e70a73199
shiras1 <shira.agron@gmail.com>
2023-09-21 23:05:23 +0300

    Added functions for user input: knock:on 10 clicks: counter decreases, picture changes reset: reset counter to 0, changes picture back to egg

--- SurpriseEgg/app/src/main/java/com/example/surpriseegg/MainActivity.java
+++ SurpriseEgg/app/src/main/java/com/example/surpriseegg/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.surpriseegg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.textview_counter);
+        counterTextView.setText(""+counter);
+    }
+
+    public void knock(View view) {
+        if(counter > 0) {
+            counter--;
+            TextView counterTextView = findViewById(R.id.textview_counter);
+            counterTextView.setText(""+counter);
+        }
+        if (counter == 0){
+            ImageView eggImageView = findViewById(R.id.imageview_egg);
+            eggImageView.setImageResource(R.drawable.surprise_egg);
+        }
+
+    }
+
+    public void reset(View view) {
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.textview_counter);
+        counterTextView.setText(""+counter);
+
+        ImageView eggImageView = findViewById(R.id.imageview_egg);
+        eggImageView.setImageResource(R.drawable.green_egg);
     }
 }
